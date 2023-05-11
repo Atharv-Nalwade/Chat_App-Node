114e8831ab63b58994f6027ba0d197da15a8f1cc
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-05-12 01:04:15 +0530

    feat: Completed chat logic

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const http = require("http");
-const socketio = require("socket.io");
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const connect = require('./config/database-config');
+
+const Chat = require('./models/chat');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const connect = require("./config/database-config.js");
+io.on('connection', (socket) => {
+    socket.on('join_room', (data) => {
+        console.log("joining a room", data.roomid)
+        socket.join(data.roomid);
+    });
 
-app.use("/", express.static(__dirname + "/public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    socket.on('msg_send', async (data) => {
+        console.log(data);
+        const chat = await Chat.create({
+            roomId: data.roomid,
+            user: data.username,
+            content: data.msg
+        });
+        io.to(data.roomid).emit('msg_rcvd', data);
+    });
 
+    socket.on('typing', (data) => {
+        socket.broadcast.to(data.roomid).emit('someone_typing');
+    })
+});
 app.set('view engine', 'ejs');
+app.use('/', express.static(__dirname + '/public'));
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  socket.on("msg_send", (data) => {
-    console.log(data);
-    io.emit("msg_rcvd", data);
-    // socket.emit('msg_rcvd', data)
-    // socket.broadcast.emit('msg_rcvd', data)
-  });
+app.get('/chat/:roomid', async (req, res) => {
+    const chats = await Chat.find({
+        roomId: req.params.roomid
+    }).select('content user');
+    console.log(chats);
+    res.render('index', {
+        name: 'Sanket',
+        id: req.params.roomid,
+        chats: chats
+    });
 });
 
-app.get('/chat/:roomid',( req, res) => {
-    res.render('index')
-})
-
 server.listen(3000, async () => {
-  console.log("Server started on port 3000");
-  await connect();
-  console.log("MongoDB connected");
+    console.log('Server started');
+    await connect();
+    console.log("mongo db connected")
 });
