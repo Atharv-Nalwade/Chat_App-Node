c5184c4c5fa313b46c5fb489ba99beb5b54ed2de
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-05-11 23:41:31 +0530

    refactor: Added ejs

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const http = require('http');
+const bodyParser = require("body-parser");
+const express = require("express");
+const http = require("http");
 const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const connect = require('./config/database-config.js');
+const connect = require("./config/database-config.js");
 
-
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+app.set('view engine', 'ejs');
 
-    socket.on('msg_send', (data) => {
-        console.log(data);
-        io.emit('msg_rcvd', data);
-        // socket.emit('msg_rcvd', data)
-        // socket.broadcast.emit('msg_rcvd', data)
-    })
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-
+  socket.on("msg_send", (data) => {
+    console.log(data);
+    io.emit("msg_rcvd", data);
+    // socket.emit('msg_rcvd', data)
+    // socket.broadcast.emit('msg_rcvd', data)
   });
+});
 
-server.listen( 3000 ,async () => {
-    console.log("Server started on port 3000");
-    await connect();
-    console.log("MongoDB connected");
-}) 
+app.get('/chat/:roomid',( req, res) => {
+    res.render('index')
+})
+
+server.listen(3000, async () => {
+  console.log("Server started on port 3000");
+  await connect();
+  console.log("MongoDB connected");
+});
